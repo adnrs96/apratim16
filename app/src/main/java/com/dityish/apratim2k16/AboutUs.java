c38ab5d30aaa8a167b87ddd286a9ea4cc48271fa
Aditya Bansal <adi.bansal241996@gmail.com>
2016-10-01 04:18:27 +0530

    Added basic infra for links to FB, Insta, Snapchat in about page :: Pending insertion of links

--- app/src/main/java/com/dityish/apratim2k16/AboutUs.java
+++ app/src/main/java/com/dityish/apratim2k16/AboutUs.java
@@ -1,20 +1,65 @@
 package com.dityish.apratim2k16;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import java.util.List;
 
 public class AboutUs extends Fragment {
 
+    ImageButton fb,insta,snap;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.about_us, container, false);
+        fb = (ImageButton)view.findViewById(R.id.imageButtonFacebook);
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(newFacebookIntent(getContext().getPackageManager(), "https://www.facebook.com/apratimccet"));
+                } catch (Exception e) {
+                    Log.d("about", "onClick: fb has issue");
+                }
+            }
+        });
+        insta = (ImageButton)view.findViewById(R.id.imageButtonInsta);
+        snap = (ImageButton)view.findViewById(R.id.imageButtonSnapchat);
+        insta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://instagram.com/_u/xxx");
+                Intent insta = new Intent(Intent.ACTION_VIEW, uri);
+                insta.setPackage("com.instagram.android");
 
+                if (isIntentAvailable(getContext(), insta)){
+                    startActivity(insta);
+                } else{
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://instagram.com/xxx")));
+                }
+            }
+        });
+        snap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String snapchatId = "";
+                Intent nativeAppIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://snapchat.com/add/" + snapchatId));
+                startActivity(nativeAppIntent);
+            }
+        });
         DisplayMetrics displaymetrics = new DisplayMetrics();
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
         int height = displaymetrics.heightPixels;
@@ -23,5 +68,24 @@ public class AboutUs extends Fragment {
         anim.con_anime().start();
 
         return view;
+    }
+
+    private boolean isIntentAvailable(Context ctx, Intent intent) {
+        final PackageManager packageManager = ctx.getPackageManager();
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    public static Intent newFacebookIntent(PackageManager pm, String url) {
+        Uri uri = Uri.parse(url);
+        try {
+            ApplicationInfo applicationInfo = pm.getApplicationInfo("com.facebook.katana", 0);
+            if (applicationInfo.enabled) {
+                // http://stackoverflow.com/a/24547437/1048340
+                uri = Uri.parse("fb://facewebmodal/f?href=" + url);
+            }
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        return new Intent(Intent.ACTION_VIEW, uri);
     }
 }
