0f224feefe52f03bb359b27c022738cb5054d63f
Aditya Bansal <adi.bansal241996@gmail.com>
2016-10-02 13:48:06 +0530

    Experiment create overlay with imagebutton over background success

--- app/src/main/java/com/dityish/apratim2k16/ProfShow.java
+++ app/src/main/java/com/dityish/apratim2k16/ProfShow.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.graphics.Rect;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -15,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -25,6 +27,9 @@ import java.util.Date;
 
 public class ProfShow extends android.support.v4.app.Fragment implements SHARED_CONSTANTS {
 
+    ImageButton show1,show2;
+
+    //-----------------------------------------
     Bitmap[] drw;
 
     Database db;
@@ -40,67 +45,26 @@ public class ProfShow extends android.support.v4.app.Fragment implements SHARED_
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_prof_show, container, false);
-        profList=(ListView) view.findViewById(R.id.ProfList);
-        profList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        show1 = (ImageButton)view.findViewById(R.id.imageButtonfortimewaste1);
+        show2 = (ImageButton)view.findViewById(R.id.imageButtonfortimewaste2);
+
+        show1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                profDetails(position);
+            public void onClick(View v) {
+                String snapchatId = "";
+                Intent nativeAppIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://snapchat.com/add/" + snapchatId));
+                startActivity(nativeAppIntent);
+            }
+        });
+        show2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String snapchatId = "";
+                Intent nativeAppIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://snapchat.com/add/" + snapchatId));
+                startActivity(nativeAppIntent);
             }
         });
 
-        drw=new Bitmap[PROF_PROFILE_PIC.length];
-
-        db=new Database(getActivity());
-
-
-        final String msg="Success";
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... params) {
-                profShows= db.getProfShows();
-                for(int i=0;i<PROF_PROFILE_PIC.length;i++)
-                {
-                    Bitmap pics= BitmapFactory.decodeResource(getResources(), PROF_PROFILE_PIC[i]);
-                    pics=getRoundedShape(pics);
-                    drw[i]=pics;
-                }
-
-                return msg;
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                DateFormat df2 = new SimpleDateFormat("hh:mm a");
-                DateFormat df = new SimpleDateFormat("dd MMM");
-                name=new String[profShows.size()];
-                location=new String[profShows.size()];
-                time=new String[profShows.size()];
-                date=new String[profShows.size()];
-                time=new String[profShows.size()];
-                desc=new String[profShows.size()];
-                reminderStart=new Date[profShows.size()];
-                reminderEnd=new Date[profShows.size()];
-                for (int i=0;i<profShows.size();i++)
-                {
-                    name[i]=profShows.get(i).getName();
-                    location[i]=profShows.get(i).getLocation();
-                    desc[i]=profShows.get(i).getDesc();
-                    date[i]=df.format(profShows.get(i).getStart());
-                    time[i]=df2.format(profShows.get(i).getStart());
-                    reminderStart[i]=profShows.get(i).getStart();
-                    reminderEnd[i]=profShows.get(i).getEnd();
-                    Log.d("Time Left",time[i]);
-                }
-
-                if (profShows.size() == 0) {
-                   Log.d("dityish says","ProfShows not received");
-
-                } else {
-                    ListAdapter custom = new ProfAdapter(getActivity(),name,drw);
-                    profList.setAdapter(custom);
-                }
-            }
-        }.execute(null, null, null);
 
         DisplayMetrics displaymetrics = new DisplayMetrics();
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
@@ -112,40 +76,4 @@ public class ProfShow extends android.support.v4.app.Fragment implements SHARED_
         return view;
     }
 
-    public Bitmap getRoundedShape(Bitmap scaleBitmapImage) {
-        int targetWidth = scaleBitmapImage.getWidth();
-        int targetHeight = scaleBitmapImage.getHeight();
-        Bitmap targetBitmap = Bitmap.createBitmap(targetWidth,
-                targetHeight, Bitmap.Config.ARGB_8888);
-
-        Canvas canvas = new Canvas(targetBitmap);
-        Path path = new Path();
-        path.addCircle(((float) targetWidth - 1) / 2,
-                ((float) targetHeight - 1) / 2,
-                (Math.min(((float) targetWidth),
-                        ((float) targetHeight)) / 2),
-                Path.Direction.CCW);
-
-        canvas.clipPath(path);
-        Bitmap sourceBitmap = scaleBitmapImage;
-        canvas.drawBitmap(sourceBitmap,
-                new Rect(0, 0, sourceBitmap.getWidth(),
-                        sourceBitmap.getHeight()),
-                new Rect(0, 0, targetWidth, targetHeight), null);
-        return targetBitmap;
-    }
-
-    public void profDetails(int profNum)
-    {
-        Intent i=new Intent(getActivity(),ProfDetails.class);
-        i.putExtra("profNum",profNum);
-        i.putExtra("name",name[profNum]);
-        i.putExtra("location",location[profNum]);
-        i.putExtra("time",time[profNum]);
-        i.putExtra("desc",desc[profNum]);
-        i.putExtra("date",date[profNum]);
-        i.putExtra("reminderStart",reminderStart[profNum].getTime());
-        i.putExtra("reminderEnd",reminderEnd[profNum].getTime());
-        startActivity(i);
-    }
 }
