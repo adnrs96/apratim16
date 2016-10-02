150a52ea2f27c191cca840cdaed77f2bfda97468
Aditya Bansal <adi.bansal241996@gmail.com>
2016-10-03 03:08:21 +0530

    Added launcher icon, cleaned up drawable, cleaned up layouts

--- app/src/main/java/com/dityish/apratim2k16/SHARED_CONSTANTS.java
+++ app/src/main/java/com/dityish/apratim2k16/SHARED_CONSTANTS.java
@@ -4,27 +4,10 @@ package com.dityish.apratim2k16;
 public interface SHARED_CONSTANTS {
 
 
-//    String [] PROF_NAMES={"PAPON","THE LOCAL TRAIN","VISHWA MOHAN BHATT & KEYBOARD SATHYA","DUALIST INQUIRY","SKRAT"};
-//
     int [] HOME_BACK_PICS={R.drawable.home5,R.drawable.home2,R.drawable.home1,R.drawable.home3,R.drawable.home4};
     int [] HOME_BACK_COLORS={R.color.home5,R.color.home2,R.color.home1,R.color.home3,R.color.home4};
 
-    int []  PROF_PROFILE_PIC={R.drawable.ranjeet_bawa_prof,
-            R.drawable.supersonic_info };
 
-    int [] PROF_PICS={R.drawable.ranjeet_bawa_info,R.drawable.supersonic_info};
-
-    String jsonEvents = "{Events:[\n" +
-            "  {\n" +
-            "    id:1,\n" +
-            "    eventName:\"Hackathon\",\n" +
-            "    location:\"CCET\",\n" +
-            "    start:\"2015-10-29T11:23\",\n" +
-            "    end:\"2015-10-29T12:23\",\n" +
-            "    desc:\"HEre goes the Description\",\n" +
-            "    isProfShow:FALSE\n" +
-            "  }\n" +
-            "  ]}";
     String [] eventsNames = { "antakshri",
             "aqua_jet",
             "arm_wrestling",
